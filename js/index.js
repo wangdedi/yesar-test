627f442dfb3fbf43727f2c4923a44a9038ef2ddc
wangdedi <546026340@qq.com>
2016-10-20 14:55:35 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-
+/*
 if (window.Event) 
 document.captureEvents(Event.MOUSEUP); 
 function nocontextmenu() 
@@ -25,4 +25,18 @@ return false;
 } 
 document.oncontextmenu = nocontextmenu; // for IE5+ 
 document.onmousedown = norightclick; // for all others 
-
+*/
+jQuery(document).ready(function($){
+			 $('#sy1').click(function(){
+			  	$('html,body').animate({scrollTop: '0px'}, 800);
+			 });
+			 $('#gy1').click(function(){
+			 	 $('html,body').animate({scrollTop:$('#gy').offset().top-90}, 800);
+			 });
+			 $('#lx1').click(function(){
+			  	$('html,body').animate({scrollTop:$('#lx').offset().top-90}, 800);
+			 });
+			 $('#jr1').click(function(){
+			  	$('html,body').animate({scrollTop:$('#jr').offset().top-90}, 800);
+			 });
+});
