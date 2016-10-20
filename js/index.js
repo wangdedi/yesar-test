8b911bcaef6f0a5be687a5e7d89f77ec183f9cb2
wangdedi <546026340@qq.com>
2016-10-20 14:59:15 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -40,3 +40,47 @@ jQuery(document).ready(function($){
 			  	$('html,body').animate({scrollTop:$('#jr').offset().top-90}, 800);
 			 });
 });
+//获取屏幕宽
+		var windWidth=$("body").width();
+		if (windWidth<=768) {
+			$("#text-gy").css("width","80%");
+			$("#text-lx").css("width","80%")
+			$("#toolR").show();
+			$("#toolR").on("click",function(){
+				$("#navbar").toggle()
+			})
+			//alert(windWidth)
+		} else {}
+		
+		$("#myCarousel").carousel('cycle');
+		$("#videoPage").css("height",$("body").height())
+		$("#videoPage1").css("height",$("body").height())
+		//视频显示隐藏
+		function videoTog(obj){
+			console.log(obj)
+			if (obj=="AR") {
+				$("#videoPage").toggle();
+			} else {
+				$("#videoPage1").toggle();
+			}
+			
+		}
+		
+		//$("#cli").on("click",function () {
+		//	$("#videoPage").show();
+		//})
+		//视频播放
+		function playPause(obj,name){ 
+			
+			var myVideo=document.getElementById(name);
+			 
+			if (myVideo.paused) {
+				$(obj).attr("class","ub-con zind999")
+				myVideo.play(); 
+			}else {
+				console.log(1235)
+				$(obj).attr("class","ub-con start zind999")
+				myVideo.pause(); 
+			}
+				  
+		} 
